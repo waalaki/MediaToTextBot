853101c92cf02fe66be17592c14c2b6b8999903a
Laki <lakicalinuur@icloud.com>
2026-01-02 10:54:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ REQUIRED_CHANNEL = os.environ.get("REQUIRED_CHANNEL", "")
 DOWNLOADS_DIR = os.environ.get("DOWNLOADS_DIR", "./downloads")
 GEMINI_KEY = os.environ.get("GEMINI_KEY", "")
 GEMINI_KEYS = os.environ.get("GEMINI_KEYS", GEMINI_KEY)
-GEMINI_MODEL = "gemini-1.5-flash"
+GEMINI_MODEL = "gemini-2.5-flash"
 
 os.makedirs(DOWNLOADS_DIR, exist_ok=True)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
